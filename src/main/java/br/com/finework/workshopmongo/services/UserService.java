2b73c967680765d3e2e38728574c245713f9e288
Nilton Dias <ntjdias@gmail.com>
2020-04-18 18:13:23 -0300

    Criando entity Post com User aninhado

--- src/main/java/br/com/finework/workshopmongo/services/UserService.java
+++ src/main/java/br/com/finework/workshopmongo/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import br.com.finework.workshopmongo.domain.User;
